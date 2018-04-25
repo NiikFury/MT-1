9a64977eb16236db0e9d6bcd8575380b77469cf0
NiikFury <niikhielrao95@github.com>
2018-04-25 14:42:10 +0530

    Setup  with LibGDX

---
+++ GhostHunters/android/src/com/Ghosthunter/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.Ghosthunter.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.Ghosthunter.game.CoreGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoreGame(), config);
+	}
+}
